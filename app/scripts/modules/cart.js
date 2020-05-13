33eee57a33e8c92f765b16dcde1ffee4c4a6ecb0
Lily Fielding <lily@lilyfielding.co.uk>
2020-05-13 16:46:00 +0100

    Added minicart

--- app/scripts/modules/cart.js
+++ app/scripts/modules/cart.js
@@ -1,19 +1,56 @@
 $(document).ready(function() {
 
+  let miniCartContentsSelector = '.js-cart--miniCart'
+
   let ajaxify = {
     onAddToCart: function(event) {
       event.preventDefault();
       $.ajax({
-          type: 'POST',
-          url: '/cart/add.js',
-          data: $(this).serialize(),
-          dataType: 'json',
-          success: ajaxify.onCartUpdated,
-          error: ajaxify.onError
+        type: 'POST',
+        url: '/cart/add.js',
+        data: $(this).serialize(),
+        dataType: 'json',
+        success: ajaxify.onCartUpdated,
+        error: ajaxify.onError
       })
     },
     onCartUpdated: function() {
-      console.log('cart-updated!')
+      let
+        $miniCartFieldset = $(miniCartContentsSelector + '.js-cart--fieldset')
+      ;
+
+      $miniCartFieldset.prop('disabled', true)
+
+      $.ajax({
+        type: 'GET',
+        url: '/cart',
+        context: document.body,
+        success: function(context) {
+          let 
+            $dataCartContents = $(context).find('.js-cart--contents'),
+            dataCartHtml = $dataCartContents.html(),
+            $dataCartItemCount = $dataCartContents.attr('data-cart-item-count'),
+            $miniCartContents = $(miniCartContentsSelector)
+            // $cartItemCount = $('.js-cart-item-count')
+          ;
+
+          $cartItemCount.text($dataCartItemCount)
+          $miniCartContents.html(dataCartHtml)
+
+          if (parseInt($dataCartItemCount) > 0) {
+            // ajaxify.openCart();
+            $('html').addClass('miniCartOpen')
+            $('.js-cart--outline').addClass('visually-hidden')
+            $('.js-cart--filled').removeClass('visually-hidden')
+          }
+          else {
+            // ajaxify.closeCart();
+            $('html').removeClass('miniCartOpen')
+            $('.js-cart--outline').removeClass('visually-hidden')
+            $('.js-cart--filled').addClass('visually-hidden')
+          }
+        }
+      })
     },
     onError: function(XMLHttpRequest, textStatus) {
       let data = XMLHttpRequest.responseJSON
